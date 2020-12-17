f3c618b4416850e2fbf93c81f1245a9be1232f91
CHRISD23 <70971173+CHRISD23@users.noreply.github.com>
2020-12-16 21:49:18 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+  clear
+  echo "Bienvenido al Instalador de Ngrok"
+  sleep 3
+  echo ""
+  echo "Detectando arquitectura...  espere puto"
+  sleep 3
+  arq=$(dpkg --print-architecture)
+  echo "Su arquitectura es ${arq}"
+  url='https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux->
+ case $arq in
+ 'arm') echo "Descargando ngrok AARCH64"
+        wget $url
+       ;;
+  'armhf') echo "Descargando Ngrok ARMHF"
+      wget $url
+        ;;
+   *) echo "Arquitevtura desconocida"
+        exit
+        ;;
+  esac
